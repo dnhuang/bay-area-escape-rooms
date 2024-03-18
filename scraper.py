be5c0e2ceac0fb84dc296b8614a5a3929b665ea3
dnhuang <dnhuang@berkeley.edu>
2024-03-17 18:54:57 -0700

    all reviews extracted

--- scraper.py
+++ scraper.py
@@ -2,11 +2,12 @@ import requests
 import re
 from bs4 import BeautifulSoup
 from textblob import TextBlob
+import time
 
 BASE_URL = 'https://www.yelp.com/biz/'
 
 class EscapeRoom:
-    def __init__(self, room_url):
+    def __init__(self, room_url, set_reviews_and_scores=False):
         self.base_url = BASE_URL
         self.room_url = room_url
         self.full_url = f'{self.base_url}{self.room_url}'
@@ -14,9 +15,13 @@ class EscapeRoom:
 
         # Abstracted intializations
         self.name, self.city, self.rating = self.set_room_info()
-        self.upper_bound = self.set_reviews_loop_upper_bound()
-        self.all_reviews = self.set_all_page_reviews()
-        self.sentiment_scores = self.compute_sentiments()
+        print(f'Creating EscapeRoom {self.name} in {self.city} with overall rating {self.rating}')
+
+        # Toggle on to scrape, otherwise false to prevent from detecing me as as bot
+        if set_reviews_and_scores:
+            self.upper_bound = self.set_reviews_loop_upper_bound()
+            self.all_reviews = self.set_all_page_reviews()
+            self.sentiment_scores = self.compute_sentiments()
     
     # Returns the upper bound for looping through the review pages.
     # Example: If the review page consists of 20 pages, then the search query will
@@ -41,7 +46,10 @@ class EscapeRoom:
         room_name = room_title_info[0].strip().lower()
 
         # Address is the 2nd elemnt in the list, city is the second to last element in list with trailing white spaces
-        room_city = room_title_info[2].split(',')[-2].strip()
+        room_city = ''
+        for title_element in room_title_info:
+            if 'California' in title_element:
+                room_city = title_element.split(',')[-2].strip()
 
         # Get the overall rating of the escape room
         overall_rating_pattern = re.compile(r'(?:[1-4](?:\.5)?|5(?:\.0)?) star rating')
@@ -65,9 +73,11 @@ class EscapeRoom:
             return page_reviews
 
         all_reviews = []
-        #### CHANGE UPPER BOUND LATER ####
+        
         print(f'total pages: {int((self.upper_bound - 1) / 10 + 1)}')
+        #### CHANGE UPPER BOUND LATER ####
         for i in range(0, self.upper_bound, 10): # loop through all review pages
+            time.sleep(10)
             print(f'appending reviews for page {int(i/10) + 1}...')
             curr_full_url = self.full_url
             if i != 0: # append appropriate search query for review page
@@ -79,7 +89,7 @@ class EscapeRoom:
 
             # .extend instead of .append because get_page_reviews returns a list
             all_reviews.extend(get_page_reviews(curr_soup_obj))
-            print(f'length of reviews: {len(all_reviews)}')
+            # print(f'length of reviews: {len(all_reviews)}')
         return all_reviews
 
     # Takes in a list of reviews and returns a list of sentiment scores corresponding in the
